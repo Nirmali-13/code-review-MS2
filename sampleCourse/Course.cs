1721d7cdf23f837982159cdd46070c027b27e396
Your Name <youremail@example.com>
2024-10-11 07:32:00 +0530

    Initial commit

---
+++ sampleCourse/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleCourse
+{
+    internal class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+    }
+}
